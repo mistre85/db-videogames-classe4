5721c93bd8d5d86b8b23f013e6b00684753a40bc
mistre <mistre85@gmail.com>
2022-11-15 11:40:52 +0100

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Data.SqlClient;
+
+
+string stringaDiConnessione = "Data Source=localhost;Initial Catalog=db-videogames;Integrated Security=True";
+
+SqlConnection connessioneSql = new SqlConnection(stringaDiConnessione);
+
+
+try
+{
+    connessioneSql.Open();
+    string insertQuery = "INSERT INTO videogames (name, overview,release_date,software_house_id) VALUES (@name, @overview,@release_date,1)";
+
+    SqlCommand insertCommand = new SqlCommand(insertQuery, connessioneSql);
+
+    insertCommand.Parameters.Add(new SqlParameter("@name", "MonkeyIsland"));
+    insertCommand.Parameters.Add(new SqlParameter("@overview", "bellissimo"));
+    insertCommand.Parameters.Add(new SqlParameter("@release_date", DateTime.Today));
+
+
+    int affectedRows = insertCommand.ExecuteNonQuery();
+
+}catch(Exception e)
+{
+    Console.WriteLine(e.Message);
+        
+}
+finally
+{
+    connessioneSql.Close();
+}
+
+
+return;
+
+
+
+
+
+
+
+
+
+
+
+try
+{
+    connessioneSql.Open();
+
+    Console.WriteLine("inserisci l'id del videogame:");
+    int id = Convert.ToInt32(Console.ReadLine());
+
+    string query = "SELECT * FROM videogames where id=@Id";
+
+    // creo il comando ed eseguo la query
+    SqlCommand cmd = new SqlCommand(query, connessioneSql);
+    cmd.Parameters.Add(new SqlParameter("@Id", id));
+   
+    //reader 
+    SqlDataReader reader = cmd.ExecuteReader();
+
+    while (reader.Read())
+    {
+        string nome = reader.GetString(1);
+        Console.WriteLine(nome);
+    }
+  
+
+}
+catch (Exception ex)
+{
+    Console.WriteLine(ex.Message);
+}
+finally
+{
+    connessioneSql.Close();
+}
---
+++ query-videogiochi.sql
@@ -0,0 +1,8 @@
+﻿--13- Selezionare le categorie dei videogame i quali hanno una media recensioni inferiore a 1.5 (10)
+SELECT DISTINCT c.id AS category_id, c.name AS category_name
+FROM videogames v
+INNER JOIN reviews r ON v.id = r.videogame_id
+INNER JOIN category_videogame cv ON v.id = cv.videogame_id
+INNER JOIN categories c ON cv.category_id = c.id
+GROUP BY v.id, c.id
+having avg(cast(r.rating as decimal))< 1.5
